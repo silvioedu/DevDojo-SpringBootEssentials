99b09837c11b7429f17116da7a3764e23b7d17f3
Silvio Silva <silvioedu@gmail.com.br>
2020-07-15 07:59:56 -0300

    adding methods e initial configaration for mysql

--- demo/src/main/java/br/com/silvio/demo/endpoint/StudentEndpoint.java
+++ demo/src/main/java/br/com/silvio/demo/endpoint/StudentEndpoint.java
@@ -1,31 +1,29 @@
 package br.com.silvio.demo.endpoint;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.silvio.demo.error.CustomErrorType;
 import br.com.silvio.demo.model.Student;
-import br.com.silvio.demo.util.DateUtil;
 
 @RestController
 @RequestMapping("students")
 public class StudentEndpoint {
 
-    private DateUtil dateUtil;
-
-    @Autowired
-    public StudentEndpoint(DateUtil dateUtil) {
-        this.dateUtil = dateUtil;
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<?> listAll(){
         return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.GET,path = "/{id}")
+    @GetMapping(path = "/{id}")
     public ResponseEntity<?> getStudentById(@PathVariable("id") int id){
         Student student = new Student();
         student.setId(id);
@@ -35,10 +33,22 @@ public class StudentEndpoint {
         return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public ResponseEntity<?> save(@RequestBody Student student){
         Student.studentList.add(student);
         return new ResponseEntity<>(student,HttpStatus.OK);
     }
+    
+    @DeleteMapping
+    public ResponseEntity<?> delete(@RequestBody Student student){
+        Student.studentList.remove(student);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody Student student){
+        Student.studentList.remove(student);
+        Student.studentList.add(student);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
